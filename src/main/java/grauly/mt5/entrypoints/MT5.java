04d331e656deed28a0cb984b4ac4b548d0b12917
Grauly <nils.dellemann@web.de>
2024-02-08 02:25:16 +0100

    feat: registered speed task to run

--- src/main/java/grauly/mt5/entrypoints/MT5.java
+++ src/main/java/grauly/mt5/entrypoints/MT5.java
@@ -4,6 +4,7 @@ import grauly.mt5.events.WeaponPullEvent;
 import grauly.mt5.events.WeaponTriggerReload;
 import grauly.mt5.registers.ModItemGroups;
 import grauly.mt5.registers.ModItems;
+import grauly.mt5.scheduler.PlayerSpeedTask;
 import grauly.mt5.scheduler.TaskScheduler;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
@@ -17,10 +18,12 @@ public class MT5 implements ModInitializer {
     public static final String MODID = "mt5";
     public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
     public static final TaskScheduler TASK_SCHEDULER = new TaskScheduler();
+    public static final PlayerSpeedTask PLAYER_SPEED_TASK = new PlayerSpeedTask();
     @Override
     public void onInitialize() {
         ModItems.registerItems();
         ModItemGroups.registerItemGroups();
+        PLAYER_SPEED_TASK.startTask(TASK_SCHEDULER,0,1);
         AttackBlockCallback.EVENT.register(WeaponTriggerReload::trigger);
         ServerEntityEvents.EQUIPMENT_CHANGE.register(WeaponPullEvent::pull);
     }
