fe34516bd2375063d65c916641dddb7af07021a1
Grauly <nils.dellemann@web.de>
2024-02-24 19:22:51 +0100

    fix: AutomaticWeapon constructor to non static CMD

--- src/main/java/grauly/mt5/weapons/AutomaticWeaponItem.java
+++ src/main/java/grauly/mt5/weapons/AutomaticWeaponItem.java
@@ -1,5 +1,6 @@
 package grauly.mt5.weapons;
 
+import eu.pb4.polymer.resourcepack.api.PolymerModelData;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -10,12 +11,13 @@ import net.minecraft.world.World;
 import java.util.function.Function;
 
 public class AutomaticWeaponItem extends WeaponItem {
-    public AutomaticWeaponItem(Settings settings, int customModelData, float maxRange, int baseDamage, float ammoSpace, int reloadTimeTicks, int shotCooldownTicks, int weaponPullCooldown, float weaponBaseSpread) {
-        super(settings, customModelData, maxRange, baseDamage, ammoSpace, reloadTimeTicks, shotCooldownTicks, weaponPullCooldown, weaponBaseSpread);
+
+    public AutomaticWeaponItem(Settings settings, PolymerModelData polymerModel, float maxRange, int baseDamage, float ammoSpace, int reloadTimeTicks, int shotCooldownTicks, int weaponPullCooldown, float weaponBaseSpread) {
+        super(settings, polymerModel, maxRange, baseDamage, ammoSpace, reloadTimeTicks, shotCooldownTicks, weaponPullCooldown, weaponBaseSpread);
     }
 
-    public AutomaticWeaponItem(Settings settings, int customModelData, float maxRange, Function<Float, Integer> damageFunction, float ammoSpace, float ammoConsumptionMultiplier, int reloadTimeTicks, int shotCooldownTicks, int weaponPullCooldown, float weaponBaseSpread) {
-        super(settings, customModelData, maxRange, damageFunction, ammoSpace, ammoConsumptionMultiplier, reloadTimeTicks, shotCooldownTicks, weaponPullCooldown, weaponBaseSpread);
+    public AutomaticWeaponItem(Settings settings, PolymerModelData polymerModel, float maxRange, Function<Float, Integer> damageFunction, float ammoSpace, float ammoConsumptionMultiplier, int reloadTimeTicks, int shotCooldownTicks, int weaponPullCooldown, float weaponBaseSpread) {
+        super(settings, polymerModel, maxRange, damageFunction, ammoSpace, ammoConsumptionMultiplier, reloadTimeTicks, shotCooldownTicks, weaponPullCooldown, weaponBaseSpread);
     }
 
     @Override
