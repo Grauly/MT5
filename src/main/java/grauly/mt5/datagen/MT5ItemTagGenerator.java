74a03cd1ee5e716e537f97b918b68f259d3caf96
Grauly <nils.dellemann@web.de>
2024-02-26 23:54:39 +0100

    feat: actual tag building & datagen run

--- src/main/java/grauly/mt5/datagen/MT5ItemTagGenerator.java
+++ src/main/java/grauly/mt5/datagen/MT5ItemTagGenerator.java
@@ -2,11 +2,16 @@ package grauly.mt5.datagen;
 
 import grauly.mt5.registers.ModItemTags;
 import grauly.mt5.registers.ModItems;
+import grauly.mt5.weapons.WeaponItem;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.TagKey;
 
 import java.util.concurrent.CompletableFuture;
+
+import static grauly.mt5.registers.ModItemTags.*;
 
 public class MT5ItemTagGenerator extends FabricTagProvider.ItemTagProvider {
     public MT5ItemTagGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
@@ -15,13 +20,26 @@ public class MT5ItemTagGenerator extends FabricTagProvider.ItemTagProvider {
 
     @Override
     protected void configure(RegistryWrapper.WrapperLookup arg) {
-        getOrCreateTagBuilder(ModItemTags.LOW_CALIBER)
+        getOrCreateTagBuilder(LOW_CALIBER)
                 .add(ModItems.BULLET_AMMO);
-        getOrCreateTagBuilder(ModItemTags.HIGH_CALIBER)
+        getOrCreateTagBuilder(HIGH_CALIBER)
                 .add(ModItems.EXPLOSION_AMMO);
 
-        getOrCreateTagBuilder(ModItemTags.PHYSICAL)
-                .addTag(ModItemTags.HIGH_CALIBER)
-                .addTag(ModItemTags.LOW_CALIBER);
+        getOrCreateTagBuilder(PHYSICAL)
+                .addTag(HIGH_CALIBER)
+                .addTag(LOW_CALIBER);
+
+        getOrCreateTagBuilder(weaponTag(ModItems.SG))
+                .addTag(PHYSICAL);
+        getOrCreateTagBuilder(weaponTag(ModItems.GLOCK))
+                .addTag(LOW_CALIBER);
+        getOrCreateTagBuilder(weaponTag(ModItems.DEAGLE))
+                .addTag(LOW_CALIBER);
+        getOrCreateTagBuilder(weaponTag(ModItems.AWP))
+                .addTag(PHYSICAL);
+    }
+
+    private TagKey<Item> weaponTag(Item weapon) {
+        return ((WeaponItem) weapon).getOrCreateAllowTag();
     }
 }
