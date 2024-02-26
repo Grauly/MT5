64ccfb2c69b9ef93ed405eb6841a98362e8c636d
Grauly <nils.dellemann@web.de>
2024-02-26 23:39:19 +0100

    feat: added overarching tags

--- src/main/java/grauly/mt5/registers/ModItemTags.java
+++ src/main/java/grauly/mt5/registers/ModItemTags.java
@@ -7,8 +7,14 @@ import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 public class ModItemTags {
-    public static final TagKey<Item> HIGH_CALIBER = TagKey.of(RegistryKeys.ITEM, new Identifier(MT5.MODID, "high_caliber"));
-    public static final TagKey<Item> LOW_CALIBER = TagKey.of(RegistryKeys.ITEM, new Identifier(MT5.MODID, "low_caliber"));
-    public static final TagKey<Item> HIGH_ENERGY = TagKey.of(RegistryKeys.ITEM, new Identifier(MT5.MODID,"high_energy"));
-    public static final TagKey<Item> LOW_ENERGY = TagKey.of(RegistryKeys.ITEM, new Identifier(MT5.MODID, "low_energy"));
+    public static final TagKey<Item> HIGH_CALIBER = tag( "high_caliber");
+    public static final TagKey<Item> LOW_CALIBER = tag( "low_caliber");
+    public static final TagKey<Item> PHYSICAL = tag("physical");
+    public static final TagKey<Item> HIGH_ENERGY = tag("high_energy");
+    public static final TagKey<Item> LOW_ENERGY = tag( "low_energy");
+    public static final TagKey<Item> ENERGY = tag("energy");
+
+    private static TagKey<Item> tag(String path) {
+        return TagKey.of(RegistryKeys.ITEM, new Identifier(MT5.MODID,path));
+    }
 }
