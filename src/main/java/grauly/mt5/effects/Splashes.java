d8d05b550710533331cade9127ae7694b5341cf2
Grauly <nils.dellemann@web.de>
2024-02-27 19:45:08 +0100

    feat: added directional splashes

--- src/main/java/grauly/mt5/effects/Splashes.java
+++ src/main/java/grauly/mt5/effects/Splashes.java
@@ -8,9 +8,17 @@ import net.minecraft.util.math.Vec3d;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Splashes {
-    public static void splash(ServerWorld world, Vec3d position, Vec3d splashNormal, ParticleEffect particle, int amount){
+    public static void splashUp(ServerWorld world, Vec3d position, ParticleEffect particle, int amount){
         for (int i = 0; i < amount; i++) {
-            Vec3d direction = splashNormal.add(ThreadLocalRandom.current().nextFloat(-0.1f,0.1f), 1,ThreadLocalRandom.current().nextFloat(-0.1f,0.1f));
+            Vec3d direction = new Vec3d(ThreadLocalRandom.current().nextFloat(-0.1f,0.1f), 1,ThreadLocalRandom.current().nextFloat(-0.1f,0.1f));
+            ParticleHelper.spawnParticle(world,particle,position,0,direction,0.5);
+        }
+    }
+
+    public static void splash(ServerWorld world, Vec3d position, Vec3d splashNormal, ParticleEffect particle, int amount) {
+        for (int i = 0; i < amount; i++) {
+            Vec3d direction = new Vec3d(ThreadLocalRandom.current().nextFloat(-0.1f,0.1f),ThreadLocalRandom.current().nextFloat(-0.1f,0.1f),  ThreadLocalRandom.current().nextFloat(-0.1f,0.1f));
+            direction = splashNormal.add(direction);
             ParticleHelper.spawnParticle(world,particle,position,0,direction,0.5);
         }
     }
