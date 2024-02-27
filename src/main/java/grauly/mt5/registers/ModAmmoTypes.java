f6f43ffc3d8521096c1e3e9391a9608a95dae4e7
Grauly <nils.dellemann@web.de>
2024-02-27 19:07:54 +0100

    chore: deleted unused file

--- src/main/java/grauly/mt5/registers/ModAmmoTypes.java
+++
@@ -1,8 +0,0 @@
-package grauly.mt5.registers;
-
-import grauly.mt5.weapons.AmmoType;
-import grauly.mt5.ammotypes.BulletAmmoType;
-
-public class ModAmmoTypes {
-    public static final AmmoType BULLET_AMMO_TYPE = new BulletAmmoType();
-}
