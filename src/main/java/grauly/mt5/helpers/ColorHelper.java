44302d70c31a9403afce9618ba7051acb41acadf
Grauly <nils.dellemann@web.de>
2024-03-05 19:48:38 +0100

    chore: auto reformating

--- src/main/java/grauly/mt5/helpers/ColorHelper.java
+++ src/main/java/grauly/mt5/helpers/ColorHelper.java
@@ -18,10 +18,10 @@ public class ColorHelper {
         float s_ = (float) Math.cbrt(s);
 
         return new Vec3d(
-                0.2104542553f*l_ + 0.7936177850f*m_ - 0.0040720468f*s_,
-                1.9779984951f*l_ - 2.4285922050f*m_ + 0.4505937099f*s_,
-                0.0259040371f*l_ + 0.7827717662f*m_ - 0.8086757660f*s_
-                );
+                0.2104542553f * l_ + 0.7936177850f * m_ - 0.0040720468f * s_,
+                1.9779984951f * l_ - 2.4285922050f * m_ + 0.4505937099f * s_,
+                0.0259040371f * l_ + 0.7827717662f * m_ - 0.8086757660f * s_
+        );
     }
 
     //Credit to https://bottosson.github.io/posts/oklab/
@@ -30,9 +30,9 @@ public class ColorHelper {
         float m_ = (float) (color.x - 0.1055613458f * color.y - 0.0638541728f * color.z);
         float s_ = (float) (color.x - 0.0894841775f * color.y - 1.2914855480f * color.z);
 
-        float l = l_*l_*l_;
-        float m = m_*m_*m_;
-        float s = s_*s_*s_;
+        float l = l_ * l_ * l_;
+        float m = m_ * m_ * m_;
+        float s = s_ * s_ * s_;
 
         return new Color(
                 +4.0767416621f * l - 3.3077115913f * m + 0.2309699292f * s,
