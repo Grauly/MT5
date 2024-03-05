cffcecd92cc142ded08f8901fcd6a622dd1869dc
Grauly <nils.dellemann@web.de>
2024-03-05 19:52:19 +0100

    chore: failed experimentation

--- src/main/java/grauly/mt5/items/TestingItem.java
+++ src/main/java/grauly/mt5/items/TestingItem.java
@@ -1,7 +1,9 @@
 package grauly.mt5.items;
 
 import eu.pb4.polymer.core.api.item.PolymerItem;
-import grauly.mt5.effects.Spheres;
+import grauly.mt5.effects.Lines;
+import grauly.mt5.helpers.ParticleHelper;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -11,6 +13,8 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.world.BlockStateRaycastContext;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -21,10 +25,11 @@ public class TestingItem extends Item implements PolymerItem {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if (world instanceof ServerWorld serverWorld) {
-            //Spheres.icoSphere(user.getEyePos(), 5, 2, p -> serverWorld.spawnParticles(ParticleTypes.END_ROD, p.x, p.y, p.z, 0, 0, 0, 0, 0));
-            Spheres.quadSphere(user.getEyePos(), 5, 64, 32, p -> serverWorld.spawnParticles(ParticleTypes.END_ROD, p.x, p.y, p.z, 0, 0, 0, 0, 0));
-        }
+        if (!(world instanceof ServerWorld serverWorld)) return TypedActionResult.fail(user.getStackInHand(hand));
+        BlockHitResult res = serverWorld.raycast(new BlockStateRaycastContext(user.getEyePos(), user.getEyePos().add(user.getRotationVector().multiply(50)), bs -> {
+            return true;
+        }));
+        Lines.line(user.getEyePos(), res.getPos(), serverWorld, ParticleTypes.END_ROD);
         return TypedActionResult.success(user.getStackInHand(hand));
     }
 
