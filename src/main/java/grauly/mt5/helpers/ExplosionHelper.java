199fb30992832952a1dafb0263b14ff32b8e56ce
Grauly <nils.dellemann@web.de>
2024-03-19 16:31:57 +0100

    feat: added shrapnel explosion

--- src/main/java/grauly/mt5/helpers/ExplosionHelper.java
+++ src/main/java/grauly/mt5/helpers/ExplosionHelper.java
@@ -3,10 +3,14 @@ package grauly.mt5.helpers;
 import grauly.mt5.entrypoints.MT5;
 import grauly.mt5.registers.ModDamageTypes;
 import grauly.mt5.registers.ModEntityTags;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
 import net.minecraft.world.explosion.ExplosionBehavior;
@@ -26,6 +30,16 @@ public class ExplosionHelper {
                 getExplosionSourceType(world));
     }
 
+    public static void shrapnelExplode(ServerWorld world, Vec3d position, float power, Entity source) {
+        world.createExplosion(source,
+                new DamageSource(world.getDamageSources().registry.entryOf(ModDamageTypes.SHRAPNEL_DAMAGE)),
+                new ShrapnelExplosionBehavior(),
+                position,
+                power,
+                false,
+                getExplosionSourceType(world));
+    }
+
     protected static World.ExplosionSourceType getExplosionSourceType(ServerWorld world) {
         return world.getGameRules().getBoolean(MT5.DESTRUCTION_ENABLED) ? World.ExplosionSourceType.BLOCK : World.ExplosionSourceType.NONE;
     }
@@ -34,6 +48,25 @@ public class ExplosionHelper {
         @Override
         public boolean shouldDamage(Explosion explosion, Entity entity) {
             return !entity.getType().isIn(ModEntityTags.MINING_CHARGE_IMMUNE);
+        }
+    }
+
+    protected static class ShrapnelExplosionBehavior extends ExplosionBehavior {
+        @Override
+        public boolean shouldDamage(Explosion explosion, Entity entity) {
+            return entity instanceof LivingEntity;
+        }
+
+        @Override
+        public boolean canDestroyBlock(Explosion explosion, BlockView world, BlockPos pos, BlockState state, float power) {
+            return false;
+        }
+
+        @Override
+        public float calculateDamage(Explosion explosion, Entity entity) {
+            float range = explosion.getPower();
+            float damage = explosion.getPower() * 1.5f;
+            return (float) (Math.log(-explosion.getPosition().distanceTo(entity.getPos()) + range) - Math.log(range) + damage);
         }
     }
 }
