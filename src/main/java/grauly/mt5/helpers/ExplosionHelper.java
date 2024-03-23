cc54e12462972ae12e3bcf2cc2286426d4c0952a
Grauly <nils.dellemann@web.de>
2024-03-23 18:38:28 +0100

    fix: explosion self damage, death messages

--- src/main/java/grauly/mt5/helpers/ExplosionHelper.java
+++ src/main/java/grauly/mt5/helpers/ExplosionHelper.java
@@ -7,9 +7,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.BlockView;
@@ -19,12 +17,12 @@ import net.minecraft.world.explosion.ExplosionBehavior;
 
 public class ExplosionHelper {
     public static void explode(ServerWorld world, Vec3d position, float power, Entity source) {
-        world.createExplosion(source, position.getX(), position.getY(), position.getZ(), power, getExplosionSourceType(world));
+        world.createExplosion(null, world.getDamageSources().explosion(null, source), null, position, power, false, getExplosionSourceType(world));
     }
 
     public static void miningExplode(ServerWorld world, Vec3d position, float power, Entity source) {
-        world.createExplosion(source,
-                new DamageSource(world.getDamageSources().registry.entryOf(ModDamageTypes.MINING_CHARGE_DAMAGE)),
+        world.createExplosion(null,
+                new DamageSource(world.getDamageSources().registry.entryOf(ModDamageTypes.MINING_CHARGE_DAMAGE), source),
                 new MiningChargeExplosionBehavior(),
                 position,
                 power,
@@ -33,7 +31,7 @@ public class ExplosionHelper {
     }
 
     public static void shrapnelExplode(ServerWorld world, Vec3d position, float power, Entity source) {
-        world.createExplosion(source,
+        world.createExplosion(null,
                 new DamageSource(world.getDamageSources().registry.entryOf(ModDamageTypes.SHRAPNEL_DAMAGE), source),
                 new ShrapnelExplosionBehavior(),
                 position,
