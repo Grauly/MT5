89bb813e430d48220c62456bb39401ec253402c3
Grauly <nils.dellemann@web.de>
2024-04-01 18:07:06 +0200

    fix: added glas panes

--- src/main/java/grauly/mt5/datagen/MT5BlockTagGenerator.java
+++ src/main/java/grauly/mt5/datagen/MT5BlockTagGenerator.java
@@ -4,8 +4,11 @@ import grauly.mt5.registers.ModBlockTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.block.Blocks;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -23,6 +26,7 @@ public class MT5BlockTagGenerator extends FabricTagProvider.BlockTagProvider {
                 .add(Blocks.DEAD_BUSH)
                 .add(Blocks.TORCH)
                 .add(Blocks.SOUL_TORCH)
+                .forceAddTag(TagKey.of(RegistryKeys.BLOCK, new Identifier("c", "glass_panes")))
                 .forceAddTag(BlockTags.LEAVES)
                 .forceAddTag(BlockTags.FLOWERS)
                 .forceAddTag(BlockTags.BAMBOO_BLOCKS)
