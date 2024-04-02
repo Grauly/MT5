7648bb187f25d0f2eb0a65a210c2403904d716cc
Grauly <nils.dellemann@web.de>
2024-04-03 01:20:49 +0200

    feat: multipurpose testing item

--- src/main/java/grauly/mt5/items/TestingItem.java
+++ src/main/java/grauly/mt5/items/TestingItem.java
@@ -1,19 +1,15 @@
 package grauly.mt5.items;
 
 import eu.pb4.polymer.core.api.item.PolymerItem;
-import grauly.mt5.effects.Lines;
 import grauly.mt5.effects.explosion.HEExplosion;
-import grauly.mt5.entrypoints.MT5;
-import grauly.mt5.helpers.MathHelper;
 import grauly.mt5.helpers.RaycastHelper;
-import grauly.mt5.scheduler.SingleRunLaterLambdaTask;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
@@ -37,7 +33,11 @@ public class TestingItem extends Item implements PolymerItem {
         Vec3d pos = result.getPos().add(0, 10, 0);
         Vec3d nor = user.getEyePos().subtract(result.getPos()).normalize();
 
-        new HEExplosion(20, result.getPos().add(0, 0.01, 0), new Vec3d(0, 1, 0), serverWorld, user).setOff();
+        if(user.isSneaking()) {
+            user.sendMessage(Text.of(String.valueOf(world.getBlockState(result.getBlockPos()).getBlock().getBlastResistance())));
+        } else {
+            new HEExplosion(4, result.getPos().add(0, 0.01, 0), new Vec3d(0, 1, 0), serverWorld, user).setOff();
+        }
         return TypedActionResult.success(user.getStackInHand(hand));
     }
 
